77f78a43c051003d835f32216094c6237f5891bb
Stanislav Skorobahatko <skorobagatko@mail.com>
2017-11-01 20:37:39 +0200

    Added id field and getters/setters for it

--- src/main/java/com/skorobahatko/library/entity/Category.java
+++ src/main/java/com/skorobahatko/library/entity/Category.java
@@ -2,10 +2,19 @@ package com.skorobahatko.library.entity;
 
 public class Category {
 
+    private long id;
     private final String name;
 
     public Category(String name) {
         this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getName() {
