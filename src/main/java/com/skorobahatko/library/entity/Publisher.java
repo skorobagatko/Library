bc9136f0c083d47e1d9f7c61e202749dcf83fcc0
Stanislav Skorobahatko <skorobagatko@mail.com>
2017-11-01 20:36:31 +0200

    Added entity class for publisher

---
+++ src/main/java/com/skorobahatko/library/entity/Publisher.java
@@ -0,0 +1,14 @@
+package com.skorobahatko.library.entity;
+
+public class Publisher {
+
+    private final String name;
+
+    public Publisher(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
