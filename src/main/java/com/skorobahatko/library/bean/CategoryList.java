9956f21f74eac9d4be9282ff1434014e445781a9
Stanislav Skorobahatko <skorobagatko@mail.com>
2017-11-01 20:42:56 +0200

    Added logic to get category by id

--- src/main/java/com/skorobahatko/library/bean/CategoryList.java
+++ src/main/java/com/skorobahatko/library/bean/CategoryList.java
@@ -3,10 +3,7 @@ package com.skorobahatko.library.bean;
 import com.skorobahatko.library.entity.Category;
 import com.skorobahatko.library.util.DatabaseUtil;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,14 +18,30 @@ public class CategoryList {
         return categories;
     }
 
+    public Category getCategoryById(String id) {
+        Category category = null;
+        Connection conn = DatabaseUtil.getConnection();
+        try (PreparedStatement stmt = conn.prepareStatement("SELECT * FROM library.category WHERE id=?;")) {
+            stmt.setString(1, id);
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+            category = new Category(rs.getString("name"));
+            category.setId(Long.parseLong(rs.getString("id")));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return category;
+    }
+
     private void getCategoriesFromDB() {
         categories = new ArrayList<>();
         Connection conn = DatabaseUtil.getConnection();
         try (Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery("SELECT * FROM library.category;")) {
             while(rs.next()) {
-                Category genre = new Category(rs.getString("name"));
-                categories.add(genre);
+                Category category = new Category(rs.getString("name"));
+                category.setId(rs.getLong("id"));
+                categories.add(category);
             }
         } catch (SQLException e) {
             e.printStackTrace();
