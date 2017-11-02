e9fd82981ccee2867bdec13f2069ee79a0259a05
Stanislav Skorobahatko <skorobagatko@mail.com>
2017-11-02 22:13:51 +0200

    Code refactoring

--- src/main/java/com/skorobahatko/library/bean/BookList.java
+++ src/main/java/com/skorobahatko/library/bean/BookList.java
@@ -1,9 +1,6 @@
 package com.skorobahatko.library.bean;
 
-import com.skorobahatko.library.entity.Author;
-import com.skorobahatko.library.entity.Book;
-import com.skorobahatko.library.entity.Category;
-import com.skorobahatko.library.entity.Publisher;
+import com.skorobahatko.library.entity.*;
 import com.skorobahatko.library.util.DatabaseUtil;
 
 import java.sql.*;
@@ -17,69 +14,99 @@ public class BookList {
 
     public List<Book> getBooks() {
         if (books == null) {
-            getBooksFromDB();
+            String sql = "SELECT b.id, b.name, b.publish_year, b.content, b.image, p.name as publisher, " +
+                    "a.name AS author, c.name AS category " +
+                    "FROM library.book b " +
+                    "INNER JOIN library.author a ON b.author_id=a.id " +
+                    "INNER JOIN library.category c ON b.category_id=c.id " +
+                    "INNER JOIN library.publisher p ON b.publisher_id=p.id;";
+            books = getBooksFromDB(sql);
         }
         return books;
     }
 
-    public List<Book> getBooksByCategory(String category) {
+    public List<Book> getBooksByCategory(String categoryId) {
         if (books != null) {
             return books.stream()
-                    .filter(b -> b.getCategory().getName().equalsIgnoreCase(category))
+                    .filter(b -> b.getCategory().getId() == Long.parseLong(categoryId))
                     .collect(Collectors.toList());
         }
-        return getBooksByCategoryFromDB(category);
+        return getBooksByCategoryFromDB(categoryId);
     }
 
-    private List<Book> getBooksByCategoryFromDB(String category) {
-        String sql = "SELECT b.name, b.publish_year, b.isbn, b.image, p.name as publisher, a.name AS author, c.name AS category " +
+    public List<Book> getBooksByAuthor(String authorId) {
+        if (books != null) {
+            return books.stream()
+                    .filter(b -> b.getAuthor().getId() == Long.parseLong(authorId))
+                    .collect(Collectors.toList());
+        }
+        return getBooksByAuthorFromDB(authorId);
+    }
+
+    public List<Book> getBooksByQuery(String query, QueryType queryType) {
+        String like = "%" + query + "%";
+        String sql = "SELECT b.id, b.name, b.publish_year, b.content, b.image, p.name as publisher, " +
+                "a.name AS author, c.name AS category " +
                 "FROM library.book b " +
                 "INNER JOIN library.author a ON b.author_id=a.id " +
                 "INNER JOIN library.category c ON b.category_id=c.id " +
                 "INNER JOIN library.publisher p ON b.publisher_id=p.id " +
-                "WHERE category_id=?";
-        List<Book> booksByCategory = new ArrayList<>();
-        Connection conn = DatabaseUtil.getConnection();
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setString(1, category);
-            ResultSet rs = stmt.executeQuery();
-            while(rs.next()) {
-                Book book = new Book();
-                book.setName(rs.getString("name"));
-                book.setIsbn(rs.getString("isbn"));
-                book.setYear(rs.getInt("publish_year"));
-                book.setImage(rs.getString("image"));
-                booksByCategory.add(book);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+                "WHERE ";
+        if (queryType == QueryType.AUTHOR) {
+            sql += "a.name LIKE '" + like + "';";
+        } else {
+            sql += "b.name LIKE '" + like + "';";
         }
-        return booksByCategory;
+        return getBooksByQueryFromDB(sql);
     }
 
-    private void getBooksFromDB() {
-        String sql = "SELECT b.name, b.publish_year, b.isbn, b.image, p.name as publisher, a.name AS author, c.name AS category " +
+    private List<Book> getBooksByQueryFromDB(String sql) {
+        return getBooksFromDB(sql);
+    }
+
+    private List<Book> getBooksByAuthorFromDB(String authorId) {
+        String sql = "SELECT b.id, b.name, b.publish_year, b.content, b.image, p.name as publisher, " +
+                "a.name AS author, c.name AS category " +
                 "FROM library.book b " +
                 "INNER JOIN library.author a ON b.author_id=a.id " +
                 "INNER JOIN library.category c ON b.category_id=c.id " +
-                "INNER JOIN library.publisher p ON b.publisher_id=p.id";
-        books = new ArrayList<>();
+                "INNER JOIN library.publisher p ON b.publisher_id=p.id " +
+                "WHERE author_id=" + authorId + ";";
+        return getBooksFromDB(sql);
+    }
+
+    private List<Book> getBooksByCategoryFromDB(String categoryId) {
+        String sql = "SELECT b.id, b.name, b.publish_year, b.content, b.image, p.name as publisher, " +
+                "a.name AS author, c.name AS category " +
+                "FROM library.book b " +
+                "INNER JOIN library.author a ON b.author_id=a.id " +
+                "INNER JOIN library.category c ON b.category_id=c.id " +
+                "INNER JOIN library.publisher p ON b.publisher_id=p.id " +
+                "WHERE category_id=" + categoryId + ";";
+        return getBooksFromDB(sql);
+    }
+
+    private List<Book> getBooksFromDB(String sql) {
+        List<Book> result = new ArrayList<>();
         Connection conn = DatabaseUtil.getConnection();
         try (Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(sql)) {
             while(rs.next()) {
                 Book book = new Book();
+                book.setId(rs.getLong("id"));
                 book.setName(rs.getString("name"));
-                book.setIsbn(rs.getString("isbn"));
                 book.setAuthor(new Author(rs.getString("author")));
                 book.setCategory(new Category(rs.getString("category")));
                 book.setPublisher(new Publisher(rs.getString("publisher")));
                 book.setYear(rs.getInt("publish_year"));
                 book.setImage(rs.getString("image"));
-                books.add(book);
+                book.setContent(rs.getString("content"));
+                result.add(book);
             }
+            rs.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return result;
     }
 }
