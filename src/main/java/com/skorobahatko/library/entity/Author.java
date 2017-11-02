cb76ce47bde17e0bec7f58921a8e76ff7b4a2dac
Stanislav Skorobahatko <skorobagatko@mail.com>
2017-11-02 22:11:34 +0200

    Added id field to author entity

--- src/main/java/com/skorobahatko/library/entity/Author.java
+++ src/main/java/com/skorobahatko/library/entity/Author.java
@@ -2,10 +2,20 @@ package com.skorobahatko.library.entity;
 
 public class Author {
 
+    private long id;
+
     private final String name;
 
     public Author(String name) {
         this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getName() {
