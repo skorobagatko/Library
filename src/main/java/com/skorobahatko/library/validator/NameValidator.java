f906622beccc05cb2dd8e50bd8790846be0a9a0a
Stanislav Skorobahatko <skorobagatko@mail.com>
2017-11-04 20:21:50 +0200

    Code refactoring

--- src/main/java/com/skorobahatko/library/validator/NameValidator.java
+++ src/main/java/com/skorobahatko/library/validator/NameValidator.java
@@ -14,18 +14,18 @@ public class NameValidator implements Validator {
     public void validate(FacesContext facesContext, UIComponent uiComponent, Object o) throws ValidatorException {
         String value = (String) o;
         if (value.trim().length() == 0) {
-            ResourceBundle resourceBundle = ResourceBundle.getBundle("messages",
-                    facesContext.getViewRoot().getLocale());
-            FacesMessage message = new FacesMessage(resourceBundle.getString("welcome_name_required"));
-            message.setSeverity(FacesMessage.SEVERITY_ERROR);
-            throw new ValidatorException(message);
+            sendError("welcome_name_required");
         }
         if (value.length() < 5) {
-            ResourceBundle resourceBundle = ResourceBundle.getBundle("messages",
-                    facesContext.getViewRoot().getLocale());
-            FacesMessage message = new FacesMessage(resourceBundle.getString("welcome_name_length_error"));
-            message.setSeverity(FacesMessage.SEVERITY_ERROR);
-            throw new ValidatorException(message);
+            sendError("welcome_name_length_error");
         }
+    }
+
+    private void sendError(String msg) {
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("messages",
+                FacesContext.getCurrentInstance().getViewRoot().getLocale());
+        FacesMessage message = new FacesMessage(resourceBundle.getString(msg));
+        message.setSeverity(FacesMessage.SEVERITY_ERROR);
+        throw new ValidatorException(message);
     }
 }
