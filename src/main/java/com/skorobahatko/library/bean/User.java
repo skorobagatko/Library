f75352ecec96cace31abc51853e4efc614511b5d
Stanislav Skorobahatko <skorobagatko@mail.com>
2017-11-12 22:08:55 +0200

    Added Serializable implementation for User class

--- src/main/java/com/skorobahatko/library/bean/User.java
+++ src/main/java/com/skorobahatko/library/bean/User.java
@@ -1,11 +1,12 @@
-package com.skorobahatko.library.entity;
+package com.skorobahatko.library.bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
 
 @ManagedBean
 @SessionScoped
-public class User {
+public class User implements Serializable {
 
     private String name;
 
