e6e732f535bd8e7987f43568d64b33c298087264
Stanislav Skorobahatko <skorobagatko@mail.com>
2017-11-12 22:07:55 +0200

    Renamed CategoryList class to CategoryController

--- src/main/java/com/skorobahatko/library/controller/CategoryController.java
+++ src/main/java/com/skorobahatko/library/controller/CategoryController.java
@@ -1,15 +1,24 @@
-package com.skorobahatko.library.bean;
+package com.skorobahatko.library.controller;
 
-import com.skorobahatko.library.entity.Category;
+import com.skorobahatko.library.bean.Category;
 import com.skorobahatko.library.util.DatabaseUtil;
 
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CategoryList {
+@ManagedBean(eager = true)
+@ApplicationScoped
+public class CategoryController implements Serializable {
 
     private List<Category> categories;
+
+    public CategoryController() {
+        getCategories();
+    }
 
     public List<Category> getCategories() {
         if (categories == null) {
